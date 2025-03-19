86b52ab01f831f2cee5cbc906f3763d44819f3fe
cagcal <c.calguner@gmail.com>
2025-03-19 19:46:23 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,73 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 7,
+   "id": "10db7066-5ef4-4861-986e-411007b8068c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import streamlit as st\n",
+    "\n",
+    "# App Title\n",
+    "st.title(\"🎤 K-Pop Singers Dashboard\")\n",
+    "st.write(\"This app provides information about K-Pop singers.\")\n",
+    "\n",
+    "# Button example\n",
+    "if st.button(\"Say Hello!\"):\n",
+    "    st.write(\"Hello! We are building a Streamlit app! 🎉\")\n",
+    "\n",
+    "# Add more widgets and features later\n",
+    "\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 9,
+   "id": "a0911559-09b9-471a-afcf-f12400661984",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "C:\\Users\\LENOVO\n"
+     ]
+    }
+   ],
+   "source": [
+    "import os\n",
+    "print(os.getcwd())\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a72052f5-9e6a-47bb-9723-f6b18969579e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.12.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
